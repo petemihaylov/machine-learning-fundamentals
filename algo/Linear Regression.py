f5bbf2ac7dbd76e1fe735d979678d56285d82604
Peter <petemihaylov@Peters-MacBook-Pro.local>
2021-02-09 14:11:10 +0100

    Saving Models & Plotting Data

--- algo/Linear Regression.py
+++ algo/Linear Regression.py
@@ -3,6 +3,9 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
@@ -12,14 +15,28 @@ predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+'''
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-linear = linear_model.LinearRegression()
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+
+    # store the model
+    if acc > best:
+        best = acc
+        print(acc)
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+'''
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
+
 
 print('Coefficient: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
@@ -28,3 +45,10 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+p = "G2"
+style.use('ggplot')
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
