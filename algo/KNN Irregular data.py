efe1bcf6a8c3bb78ead0a64ba0dc381dcf77d1b9
Peter <petemihaylov@Peters-MacBook-Pro.local>
2021-02-10 18:03:50 +0100

    Predicts the data with KNeighborsClassifier with accuracy of 0.94

--- algo/KNN Irregular data.py
+++ algo/KNN Irregular data.py
@@ -28,4 +28,16 @@ y = list(cls)
 
 x_train, x_test, y_train, y_test =  sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
+model = KNeighborsClassifier(n_neighbors=7)
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
 
+print(acc)
+
+predicted = model.predict(x_test)
+names = ['uacc', 'acc', 'good', 'vgood']
+
+for x in range( len(predicted)):
+    print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+    n = model.kneighbors([x_test[x]], 9, True)
+    #print("N: ", n)
